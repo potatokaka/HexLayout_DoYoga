22d4290bdd83ab92975b5315b5ef2ee5e1c8a2df
potatokaka <potatokaka@gmail.com>
2021-08-22 11:13:46 +0800

    Update 2021-08-22T03:13:40.537Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,34 +1,35 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+"use strict";
 
-// Swiper
+$(function () {
+  console.log('Hello Bootstrap5');
+}); // Swiper
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
-
   // 768px 以上
   breakpoints: {
-    768:{
+    768: {
       slidesPerView: 3,
-      spaceBetween: 30,
-    },
+      spaceBetween: 30
+    }
   }
-  
   /* 移除 pagination */
   // pagination: {
   //   el: ".swiper-pagination",
   //   clickable: true,
   // },
-});
 
-
-// AOS Animation
-
+}); // AOS Animation
 //AOS.init();
+
 AOS.init({
   once: true,
-  offset: 400, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
+  offset: 400,
+  // offset (in px) from the original trigger point
+  delay: 0,
+  // values from 0 to 3000, with step 50ms
   duration: 1000 // values from 0 to 3000, with step 50ms
+
 });
+//# sourceMappingURL=all.js.map
